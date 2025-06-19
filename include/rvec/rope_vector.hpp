4982082e03712f0390079d02bb23e92a23e55bdd
onatbaker <onatbaker@gmail.com>
2025-06-19 22:03:29 +0300

    formatting

--- include/rvec/rope_vector.hpp
+++ include/rvec/rope_vector.hpp
@@ -5,10 +5,12 @@
 #include <cassert>
 #include <stdexcept>
 
-namespace rvec {
+namespace rvec
+{
 
     template <typename T, std::size_t ChunkSize = 256>
-    class rope_vector {
+    class rope_vector
+    {
     public:
         using value_type = T;
         using size_type = std::size_t;
@@ -17,11 +19,20 @@ namespace rvec {
         std::vector<std::unique_ptr<T[]>> chunks;
         size_type total_size = 0;
 
-        static constexpr size_type chunk_index(size_type i) { return i / ChunkSize; }
-        static constexpr size_type within_chunk_index(size_type i) { return i % ChunkSize; }
+        static constexpr size_type chunk_index(size_type i)
+        {
+            return i / ChunkSize;
+        }
 
-        void ensure_capacity_for(size_type i) {
-            while (i >= chunks.size() * ChunkSize) {
+        static constexpr size_type within_chunk_index(size_type i)
+        {
+            return i % ChunkSize;
+        }
+
+        void ensure_capacity_for(size_type i)
+        {
+            while (i >= chunks.size() * ChunkSize)
+            {
                 chunks.emplace_back(std::make_unique<T[]>(ChunkSize));
             }
         }
@@ -29,93 +40,147 @@ namespace rvec {
     public:
         rope_vector() = default;
 
-        size_type size() const noexcept { return total_size; }
+        size_type size() const noexcept
+        {
+            return total_size;
+        }
 
-        bool empty() const noexcept { return total_size == 0; }
+        bool empty() const noexcept
+        {
+            return total_size == 0;
+        }
 
-        T& operator[](size_type i) {
+        T& operator[](size_type i)
+        {
             assert(i < total_size);
             return chunks[chunk_index(i)][within_chunk_index(i)];
         }
 
-        const T& operator[](size_type i) const {
+        const T& operator[](size_type i) const
+        {
             assert(i < total_size);
             return chunks[chunk_index(i)][within_chunk_index(i)];
         }
 
-        T& at(size_type i) {
-            if (i >= total_size) throw std::out_of_range("rvec::at() index out of range");
+        T& at(size_type i)
+        {
+            if (i >= total_size)
+            {
+                throw std::out_of_range("rvec::at() index out of range");
+            }
             return (*this)[i];
         }
 
-        const T& at(size_type i) const {
-            if (i >= total_size) throw std::out_of_range("rvec::at() index out of range");
+        const T& at(size_type i) const
+        {
+            if (i >= total_size)
+            {
+                throw std::out_of_range("rvec::at() index out of range");
+            }
             return (*this)[i];
         }
 
-        T& front() {
-            if (empty()) throw std::out_of_range("rvec::front() called on empty vector");
+        T& front()
+        {
+            if (empty())
+            {
+                throw std::out_of_range("rvec::front() called on empty vector");
+            }
             return (*this)[0];
         }
 
-        const T& front() const {
-            if (empty()) throw std::out_of_range("rvec::front() called on empty vector");
+        const T& front() const
+        {
+            if (empty())
+            {
+                throw std::out_of_range("rvec::front() called on empty vector");
+            }
             return (*this)[0];
         }
 
-        T& back() {
-            if (empty()) throw std::out_of_range("rvec::back() called on empty vector");
+        T& back()
+        {
+            if (empty())
+            {
+                throw std::out_of_range("rvec::back() called on empty vector");
+            }
             return (*this)[total_size - 1];
         }
 
-        const T& back() const {
-            if (empty()) throw std::out_of_range("rvec::back() called on empty vector");
+        const T& back() const
+        {
+            if (empty())
+            {
+                throw std::out_of_range("rvec::back() called on empty vector");
+            }
             return (*this)[total_size - 1];
         }
 
-        void clear() {
+        void clear()
+        {
             total_size = 0;
         }
 
-        void resize(size_type new_size) {
-            if (new_size < total_size) {
+        void resize(size_type new_size)
+        {
+            if (new_size < total_size)
+            {
                 total_size = new_size;
             }
-            else {
+            else
+            {
                 ensure_capacity_for(new_size - 1);
-                for (size_type i = total_size; i < new_size; ++i) {
+                for (size_type i = total_size; i < new_size; ++i)
+                {
                     chunks[chunk_index(i)][within_chunk_index(i)] = T{};
                 }
                 total_size = new_size;
             }
         }
 
-        void push_back(const T& value) {
+        void push_back(const T& value)
+        {
             ensure_capacity_for(total_size);
             chunks[chunk_index(total_size)][within_chunk_index(total_size)] = value;
             ++total_size;
         }
 
-        void push_back(T&& value) {
+        void push_back(T&& value)
+        {
             ensure_capacity_for(total_size);
             chunks[chunk_index(total_size)][within_chunk_index(total_size)] = std::move(value);
             ++total_size;
         }
 
-        void insert(size_type pos, const T& value) {
-            if (pos > total_size) throw std::out_of_range("insert position out of bounds");
+        void insert(size_type pos, const T& value)
+        {
+            if (pos > total_size)
+            {
+                throw std::out_of_range("insert position out of bounds");
+            }
+
             push_back(T{}); // make space
-            for (size_type i = total_size - 1; i > pos; --i) {
+
+            for (size_type i = total_size - 1; i > pos; --i)
+            {
                 (*this)[i] = std::move((*this)[i - 1]);
             }
+
             (*this)[pos] = value;
         }
 
-        void erase(size_type pos) {
-            if (pos >= total_size) throw std::out_of_range("erase position out of bounds");
-            for (size_type i = pos; i < total_size - 1; ++i) {
+        void erase(size_type pos)
+        {
+            if (pos >= total_size)
+            {
+                throw std::out_of_range("erase position out of bounds");
+            }
+
+            for (size_type i = pos; i < total_size - 1; ++i)
+            {
                 (*this)[i] = std::move((*this)[i + 1]);
             }
+
             --total_size;
         }
     };
